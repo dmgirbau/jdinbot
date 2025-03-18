2e03a2293244542efca25fd12950f9fcbb918f63
JoanManuel16 <altaircuba@gmail.com>
2025-03-18 19:11:02 -0400

    new main config

---
+++ src/main.py
@@ -0,0 +1,42 @@
+import logging
+
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+from src.bot.comands import start, transfer, balance
+from src.config import AppConfig
+from src.db.dbconfiguration import DataBase
+
+config = AppConfig()
+TELEGRAM_TOKEN = config.TELEGRAM_TOKEN
+APP_MODE = config.APP_MODE
+
+# Logger configuration
+if APP_MODE == "dev":
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    )
+else:
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.ERROR
+    )
+
+logger = logging.getLogger(__name__)
+
+def main():
+    database = DataBase()
+    database.__init__()
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("transfer", transfer))
+    app.add_handler(CommandHandler("balance", balance))
+
+    # Start the bot
+    logger.info("Polling...")
+    app.run_polling()
+    logger.info("Bot stopped")
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.error(f"Error: {e}")
